79a361badd71c75ce142b94489222e16801ac98b
YohanGrember <yohan.grember@gmail.com>
2017-01-11 22:55:59 +0100

    Update mnist_deep.py

--- mnist_deep.py
+++ mnist_deep.py
@@ -1,27 +1,32 @@
+# -*- coding: utf-8 -*-
+
 #This script train and test a deep CNN on MNIST database, 
 #using entropyloss / multiclasshingeloss / crammerloss / leeloss(not finished)
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
 import argparse
-import random
+#####import random
 import time 
-
 # Import data
 from tensorflow.examples.tutorials.mnist import input_data
-
 import tensorflow as tf
 import numpy as np
 
 #choose parameters
-LOSS=1 # 0:entropyloss  1:multiclasshingeloss 2:multiclasscrammerloss(sous contrainte b=0) 3:multiclassleeloss(sum(b)=0??)
-BATCH_SIZE=30
-ITERATION=1000
-LAMB=0 #devant le terme de régularization
-LEARNING_RATE=1e-4
+LOSS=0 # 0:entropyloss  1:multiclasshingeloss 2:multiclasscrammerloss(sous contrainte b=0) 3:multiclassleeloss(sum(b)=0??)
+BATCH_SIZE=50
+ITERATION=3000
+LAMBDA=5e-5 #devant le terme de régularisation
 FLAGS = None
+
+# Set up the learning rate : decayed_learning_rate = INITIAL_LEARNING_RATE * DECAY_RATE ^ (global_step / DECAY_STEPS)
+INITIAL_LEARNING_RATE=1e-4
+DECAY_STEPS = 500
+DECAY_RATE = 0.85
+
+
 
 def entropyloss(scores,classes):
   return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(scores, classes))
@@ -65,8 +70,14 @@ def multiclassleeloss(scores,classes,nb_class):
   return final_loss
   
 
-def regularization_loss(lamb,weights):
-  return lamb*tf.reduce_sum(tf.square(weights)) 
+#def regularization_loss(lamb,weights):
+#  return lamb*tf.reduce_sum(tf.square(weights)) 
+  
+def regularization_loss(lamb,weights_list):
+  loss = 0  
+  for weights in weights_list:
+    loss += lamb*tf.cast(tf.reduce_sum(tf.square(weights)),tf.float32)
+  return loss
 
 def transformData(data): #from [1 3 2 2 1] to [[1 0 0] [0 1 0] ...]
   batch_yss=np.tile(np.transpose([data]),[1,3])
@@ -97,7 +108,6 @@ def main(_):
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
   x=tf.placeholder(tf.float32,[None,784])
-
   x_image=tf.reshape(x,[-1,28,28,1])
 
   #first layer
@@ -132,21 +142,28 @@ def main(_):
   y_ = tf.placeholder(tf.float32, [None, 10])
 
   #define the loss and gradient descent
+  weights_list = [W_conv1, W_conv2, W_fc1, W_fc2]
+
   if LOSS==0:
     y = tf.matmul(h_fc1_drop,W_fc2)+b_fc2
-    svm_loss=entropyloss(y,y_) + regularization_loss(LAMB,W_fc2)
+    svm_loss=entropyloss(y,y_) + regularization_loss(LAMBDA,weights_list)
   elif LOSS==1:
     y= normalization(tf.matmul(h_fc1_drop,W_fc2)+b_fc2)
-    svm_loss = multiclasshingeloss(y,y_) + regularization_loss(LAMB,W_fc2)
+    svm_loss = multiclasshingeloss(y,y_) + regularization_loss(LAMBDA,weights_list)
   elif LOSS==2:
     y = normalization(tf.matmul(h_fc1_drop,W_fc2))
-    svm_loss=multiclasscrammerloss(y,y_) + regularization_loss(LAMB,W_fc2)
+    svm_loss=multiclasscrammerloss(y,y_) + regularization_loss(LAMBDA,weights_list)
   else:
     y = normalization(tf.matmul(h_fc1_drop,W_fc2) + b_fc2)
-    svm_loss=multiclassleeloss(y,y_,10) + regularization_loss(LAMB,W_fc2)
+    svm_loss=multiclassleeloss(y,y_,10) + regularization_loss(LAMBDA,weights_list)
 
-  train_step = tf.train.AdamOptimizer(LEARNING_RATE).minimize(svm_loss)
+  global_step = tf.Variable(0, trainable=False)
+  # Introducing a decaying learning rate
+  learning_rate = tf.train.exponential_decay(INITIAL_LEARNING_RATE, global_step, DECAY_STEPS, DECAY_RATE)
+  train_step = tf.train.AdamOptimizer(learning_rate).minimize(svm_loss, global_step=global_step)
 
+  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
   sess = tf.InteractiveSession()
   tf.global_variables_initializer().run()
 
@@ -155,7 +172,17 @@ def main(_):
   #TRAIN
   for k in range(ITERATION):
     batch = mnist.train.next_batch(BATCH_SIZE)
-    sess.run(train_step,feed_dict={x: batch[0], y_: batch[1], keep_prob: 1})
+    if k%100 == 0:
+      train_accuracy = accuracy.eval(feed_dict={
+        x:batch[0], y_:batch[1], keep_prob: 1.0})
+      print("step %d, learning_rate %g, training accuracy %g"%(k, learning_rate.eval(), train_accuracy))
+      if k%1000 == 0:
+        print("nombre d'epochs utilisés %d"%mnist.train._epochs_completed)  
+     # weights_norm = 0  
+     # for weights in weights_list:
+     #   weights_norm += 1*tf.cast(tf.reduce_sum(tf.square(weights)),tf.float32)
+     # print("norm of weights %g" %(weights_norm))
+    sess.run(train_step,feed_dict={x: batch[0], y_: batch[1], keep_prob: .5})
 
   tps2 = time.clock() 
   print("weights = ")
@@ -167,9 +194,7 @@ def main(_):
   print("temps training = ",int(tps2 - tps1)," secondes" )
 
   # TEST
-  correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  teste = mnist.test.next_batch(2000)
+  teste = mnist.test.next_batch(5000)
   print("risque réel = ",sess.run(accuracy, feed_dict={x:teste[0], y_: teste[1], keep_prob: 1.0}))
 
 

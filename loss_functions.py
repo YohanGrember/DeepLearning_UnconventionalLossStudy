02ff56ea806f6b415f80264fa72736c863b17f2e
QizhouXING <eric08525252@gmail.com>
2017-03-23 11:08:39 +0100

    Add files via upload

--- loss_functions.py
+++ loss_functions.py
@@ -4,10 +4,10 @@ def normalization(scores):
   return scores-tf.tile(tf.transpose([tf.reduce_mean(scores,1)]),[1,tf.cast(scores.get_shape()[1], dtype=tf.int32)])
 
 def regularization_loss(lamb,weights):
-  return lamb*tf.reduce_sum(tf.square(weights)) 
+  return lamb*tf.reduce_sum(tf.square(weights))
 
 def entropyloss(scores,classes):
-  return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(scores, classes))
+  return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=scores, labels=classes))
 
 def large_margin_scores(basic_scores,weights):
   weight_norm=tf.sqrt(tf.reduce_sum(tf.square(weights),0))
@@ -22,19 +22,17 @@ def large_margin_scores(basic_scores,weights):
   return tf.multiply(oo,norm)
 
 def multiclasshingeloss(scores, classes,batch_size):
-  scores=tf.transpose(scores)
+  scores=tf.transpose(normalization(scores))
   classes=tf.transpose(classes)
   true_classes = tf.argmax(classes, 0)
   idx_flattened = tf.range(0, batch_size) * scores.get_shape()[0]+ tf.cast(true_classes, dtype=tf.int32)
   true_scores = tf.gather(tf.reshape(tf.transpose(scores), [-1]),
                           idx_flattened)
   L = tf.nn.relu((1 - true_scores + scores) * (1 - classes))
-  l=tf.reduce_sum(L,0)
-  final_loss=tf.reduce_mean(l)
-  return final_loss
+  return tf.reduce_mean(tf.reduce_sum(L,0))
 
 def multiclasscrammerloss(scores, classes,batch_size):
-  scores=tf.transpose(scores)
+  scores=tf.transpose(normalization(scores))
   classes=tf.transpose(classes)
   true_classes = tf.argmax(classes, 0)
   idx_flattened = tf.range(0, batch_size) * scores.get_shape()[0]+ tf.cast(true_classes, dtype=tf.int32)
@@ -45,7 +43,7 @@ def multiclasscrammerloss(scores, classes,batch_size):
   return final_loss
 
 def multiclassleeloss(scores,classes,nb_class,batch_size):
-  scores=tf.transpose(scores)
+  scores=tf.transpose(normalization(scores))
   classes=tf.transpose(classes)
   true_classes = tf.argmax(classes, 0)
   idx_flattened = tf.range(0, batch_size) * scores.get_shape()[0]+ tf.cast(true_classes, dtype=tf.int32)
@@ -56,55 +54,83 @@ def multiclassleeloss(scores,classes,nb_class,batch_size):
   final_loss=tf.reduce_mean(l)
   return final_loss
 
-
-def surrogateloss(scores, classes,batch_size):
-  scores = tf.transpose(scores)
+def surrogate_hinge(scores, classes,batch_size):
+  scores = tf.transpose(normalization(scores))
   classes = tf.transpose(classes)
-  cost = 1;
-  true_classes = tf.argmax(classes, 0)
-  idx_flattened = tf.range(0, batch_size) * scores.get_shape()[0] + tf.cast(true_classes, dtype=tf.int32)
-  true_scores = tf.gather(tf.reshape(tf.transpose(scores), [-1]),
-                          idx_flattened)
-  L = cost * tf.nn.relu(1 - true_scores + scores)
+  L = tf.nn.relu((1 + scores)*(1-classes))
   l = tf.reduce_sum(L, 0)
   final_loss = tf.reduce_mean(l)
   return final_loss
 
-def GLE(scores, classes, nb_class,batch_size):
-  scores = tf.transpose(scores)
+def surrogate_hinge_squares(scores, classes,batch_size):
+  scores = tf.transpose(normalization(scores))
   classes = tf.transpose(classes)
-  cost = [[1.0 for x in range(nb_class)] for x in range(nb_class)]
-  for i in range(nb_class) :
-    for j in range(nb_class):
-      if i==j:
-          cost[i][j]=0.0
+  L = tf.nn.relu((1 + scores)*(1-classes))
+  l = tf.reduce_sum(tf.square(L), 0)
+  final_loss = tf.reduce_mean(l)
+  return final_loss
 
-  true_classes = tf.argmax(classes, 0)
-  idx_flattened = tf.range(0, batch_size) * scores.get_shape()[0] + tf.cast(true_classes, dtype=tf.int32)
-  true_scores = tf.gather(tf.reshape(tf.transpose(scores), [-1]),
-                          idx_flattened)
-  L = tf.matmul(cost, tf.exp(scores - true_scores))
-  #L = cost * tf.exp(scores - true_scores)
-  #L = tf.exp(scores - true_scores)
+def surrogate_squares(scores, classes,batch_size):
+  scores = tf.transpose(normalization(scores))
+  classes = tf.transpose(classes)
+  L = (1 + scores)*(1-classes)
+  l = tf.reduce_sum(tf.square(L), 0)
+  final_loss = tf.reduce_mean(l)
+  return final_loss
+
+def surrogate_exponential(scores, classes,batch_size):
+  scores = tf.transpose(normalization(scores))
+  classes = tf.transpose(classes)
+  L = tf.exp(scores)*(1-classes)
   l = tf.reduce_sum(L, 0)
   final_loss = tf.reduce_mean(l)
   return final_loss
 
-def GLL(scores, classes, nb_class,batch_size):
-  scores = tf.transpose(scores)
+def surrogate_sigmoid(scores, classes,batch_size):
+  scores = tf.transpose(normalization(scores))
   classes = tf.transpose(classes)
-  cost = [[1.0 for x in range(nb_class)] for x in range(nb_class)]
-  for i in range(nb_class) :
-    for j in range(nb_class):
-      if i==j:
-          cost[i][j]=0.0
-
-  true_classes = tf.argmax(classes, 0)
-  idx_flattened = tf.range(0, batch_size) * scores.get_shape()[0] + tf.cast(true_classes, dtype=tf.int32)
-  true_scores = tf.gather(tf.reshape(tf.transpose(scores), [-1]),
-                          idx_flattened)
-  L = tf.matmul(cost, tf.exp(scores - true_scores))
+  L = (1+tf.tanh(scores))*(1-classes)
   l = tf.reduce_sum(L, 0)
   final_loss = tf.reduce_mean(l)
-  final_loss2 = tf.log(1+final_loss)
-  return final_loss2
+  return final_loss
+
+def surrogate_logistic(scores, classes,batch_size):
+  scores = tf.transpose(normalization(scores))
+  classes = tf.transpose(classes)
+  L = tf.log(1+tf.exp(scores))*(1-classes)
+  l = tf.reduce_sum(L, 0)
+  final_loss = tf.reduce_mean(l)
+  return final_loss
+
+def surrogate_double_hinge(scores, classes,batch_size):
+  scores = tf.transpose(normalization(scores))
+  classes = tf.transpose(classes)
+  L = tf.subtract(tf.nn.relu(1 + scores),tf.nn.relu(scores))*(1-classes)
+  l = tf.reduce_sum(L, 0)
+  final_loss = tf.reduce_mean(l)
+  return final_loss
+
+def GEL(scores, classes, batch_size):
+  scores=tf.transpose(normalization(scores))
+  #scores=tf.cast(scores, tf.float64)
+  classes=tf.transpose(classes)
+  true_classes = tf.argmax(classes, 0)
+  idx_flattened = tf.range(0, batch_size) * scores.get_shape()[0]+ tf.cast(true_classes, dtype=tf.int32)
+  true_scores = tf.gather(tf.reshape(tf.transpose(scores), [-1]),
+                          idx_flattened)
+  L = tf.exp(scores - true_scores)*(1 - classes)
+  l = tf.reduce_sum(L, 0)
+  final_loss = tf.reduce_mean(l)
+  return final_loss
+
+def GLL(scores, classes, batch_size):
+  scores=tf.transpose(normalization(scores))
+  classes=tf.transpose(classes)
+  true_classes = tf.argmax(classes, 0)
+  idx_flattened = tf.range(0, batch_size) * scores.get_shape()[0]+ tf.cast(true_classes, dtype=tf.int32)
+  true_scores = tf.gather(tf.reshape(tf.transpose(scores), [-1]),
+                          idx_flattened)
+  L = tf.exp(scores - true_scores)*(1 - classes)
+  l = tf.log1p(tf.reduce_sum(L, 0))
+  final_loss = tf.reduce_mean(l)
+  return final_loss
